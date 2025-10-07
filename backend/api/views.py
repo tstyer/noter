79d4dc0f30efe4e0733578ede48a64371d4c6381
tstyer <travisstyer.ts@gmail.com>
2025-10-07 18:42:59 +0100

    Add: note add view

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,8 +3,25 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated] #You cannot call the class unless you have a valid jwt token. 
+
+    def get_queryset(self):
+        user = self.request.user         # Select the right user. 
+        return Note.objects.filter(author=user) # Filter method to get the right user, not all posts. 
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
 
 # Chosen the generic view built into django that will handle creating a new user object. 
 # The below will look at all the users to as to not duplicate,
