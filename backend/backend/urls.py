40922a31bbfb77a21f3a1d6e37d658ce8ac1b9c1
tstyer <travisstyer.ts@gmail.com>
2025-10-18 11:25:28 +0100

    add health check to backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,11 @@ from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def health(request):
+    return HttpResponse("Backend is working")
+
 urlpatterns = [
+    path("", health, name="health"),
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
