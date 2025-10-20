88b3ce32cab8721bcdcfa12c589096bd4ff34491
tstyer <travisstyer.ts@gmail.com>
2025-10-20 16:20:35 +0100

    Change const api in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,7 +6,8 @@ const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 const api = axios.create({
     // Allows me to import anything specified inside an env. variable file. 
     // It will import the url created in the .env file in frontend. 
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL || "",   // "" => same origin (https://<app>/)
+  headers: { "Content-Type": "application/json" }
 });
 
 api.interceptors.request.use(
